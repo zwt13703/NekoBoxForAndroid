cda108047d47612ed53e6e56ed00a87d12879caa
zhouwentao <zwt13703@gmail.com>
2026-02-28 20:34:40 +0800

    feat:修改图标，包名

--- app/src/main/java/io/nekohasekai/sagernet/Constants.kt
+++ app/src/main/java/io/nekohasekai/sagernet/Constants.kt
@@ -183,5 +183,5 @@ object Action {
     const val RELOAD = "io.nekohasekai.sagernet.RELOAD"
 
     // const val SWITCH_WAKE_LOCK = "io.nekohasekai.sagernet.SWITCH_WAKELOCK"
-    const val RESET_UPSTREAM_CONNECTIONS = "moe.nb4a.RESET_UPSTREAM_CONNECTIONS"
+    const val RESET_UPSTREAM_CONNECTIONS = "wkk.fastbox.RESET_UPSTREAM_CONNECTIONS"
 }
